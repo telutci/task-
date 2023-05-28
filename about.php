1a3f1fadd154d7deecd837accd8ab55d8739d1f3
telutci <133240213+telutci@users.noreply.github.com>
2023-05-28 18:10:50 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php include 'header.php'; ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="container konten-wrapper">
+			<div class="panel panel-default">
+				<div class="panel-body main">
+					<div class="col-md-8">
+						<div class="about">
+							<div class="row post-title">
+								<div class="col-sm-12">
+									<h2>TENTANG BERITA TERBARU</h2>
+								</div>
+							</div>
+							<div class="row post-content">
+								<div class="col-sm-12">
+									<p>Tentang Berita terbaru sebuah platform berita yang berfokus pada memberikan informasi terkini dan relevan mengenai berbagai topik yang sedang dibicarakan di dalam dan nasional. Platform ini bertujuan untuk memberikan pemahaman yang baik kepada pembaca tentang berbagai peristiwa, tren, dan isu-isu penting yang mempengaruhi masyarakat. Berita Kita didukung oleh tim jurnalis yang berkompeten dan berpengalaman dalam bidang jurnalisme. Mereka melakukan riset mendalam, wawancara, dan analisis untuk menyajikan berita yang akurat dan dapat dipercaya kepada pembaca. Redaksi Berita Kita juga memastikan bahwa berita-berita yang disajikan bersifat netral, adil, dan tidak memihak, sehingga pembaca dapat membentuk opini mereka sendiri berdasarkan informasi yang objektif.</p>
+									<p>Platform ini mengabdi untuk memenuhi kebutuhan informasi masyarakat dengan menyajikan berita dalam berbagai kategori, seperti Game, teknologi, kesehatan Dan lain-lain. Dalam setiap berita, Berita Kita berusaha memberikan konteks yang diperlukan agar pembaca dapat memahami peristiwa tersebut secara menyeluruh. Selain itu, platform ini juga berkomitmen untuk menyajikan berita yang bermanfaat dan edukatif bagi pembaca. Selain artikel berita, Berita Kita juga menyediakan berbagai format lain untuk memberikan pengalaman yang lebih interaktif dalam mengonsumsi informasi. Pembaca juga dapat berpartisipasi dengan memberikan tanggapan, komentar, atau berbagi artikel ke media sosial untuk memperluas jangkauan berita yang disajikan.</p>
+									<p>Dalam era di mana informasi tersebar dengan cepat, Berita berkomitmen untuk menjadi sumber berita yang terpercaya dan bertanggung jawab. Platform ini bertujuan untuk memberikan informasi yang berkualitas, meningkatkan literasi media masyarakat, dan mempromosikan pemahaman yang mendalam tentang isu-isu penting yang ada di sekitar kita. Itulah penjelasan mengenai Berita Kita sebagai platform berita yang memberikan informasi terkini dan beragam dengan komitmen pada akurasi, netralitas, dan memberikan pemahaman yang baik kepada pembaca.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php include 'sidebar.php'; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'footer.php';
